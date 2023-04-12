dc52de7f43ae156b3c0c0636856561e123502ddf
Felix Divo <felix.divo@cs.tu-darmstadt.de>
2023-04-12 14:20:23 +0200

    Adjust training

--- code/TFC/main.py
+++ code/TFC/main.py
@@ -8,7 +8,7 @@ from utils import _logger
 from model import *
 from dataloader import data_generator
 from trainer import Trainer
-
+from rtpt import RTPT
 
 
 
@@ -22,7 +22,7 @@ parser.add_argument('--run_description', default='run1', type=str,
 parser.add_argument('--seed', default=42, type=int, help='seed value')
 
 # 1. self_supervised pre_train; 2. finetune (itself contains finetune and test)
-parser.add_argument('--training_mode', default='fine_tune_test', type=str,
+parser.add_argument('--training_mode', default='pre_train', type=str,
                     help='pre_train, fine_tune_test')
 
 parser.add_argument('--pretrain_dataset', default='SleepEEG', type=str,
@@ -30,17 +30,20 @@ parser.add_argument('--pretrain_dataset', default='SleepEEG', type=str,
 parser.add_argument('--target_dataset', default='Epilepsy', type=str,
                     help='Dataset of choice: Epilepsy, FD_B, Gesture, EMG')
 
-parser.add_argument('--logs_save_dir', default='../experiments_logs', type=str,
+parser.add_argument('--logs_save_dir', default='../exp_logs', type=str,
                     help='saving directory')
 parser.add_argument('--device', default='cuda', type=str,
                     help='cpu or cuda')
+parser.add_argument('--device_id', default='0', type=int,
+                    help='cuda device id')
 parser.add_argument('--home_path', default=home_dir, type=str,
                     help='Project home directory')
 args, unknown = parser.parse_known_args()
 
 with_gpu = torch.cuda.is_available()
 if with_gpu:
-    device = torch.device("cuda")
+    device = torch.device(f"cuda:{args.device_id}")
+    print(device)
 else:
     device = torch.device("cpu")
 print('We are using %s now.' %device)
@@ -55,7 +58,7 @@ training_mode = args.training_mode
 run_description = args.run_description
 logs_save_dir = args.logs_save_dir
 os.makedirs(logs_save_dir, exist_ok=True)
-exec(f'from config_files.{pretrain_dataset}_Configs import Config as Configs')
+exec(f'from config_files.{pretrain_dataset.replace("-","_")}_Configs import Config as Configs')
 configs = Configs()
 
 # # ##### fix random seeds for reproducibility ########
@@ -89,11 +92,16 @@ logger.debug("=" * 45)
 # Load datasets
 sourcedata_path = f"../../datasets/{pretrain_dataset}"
 targetdata_path = f"../../datasets/{targetdata}"
-subset = True  # if subset= true, use a subset for debugging.
+subset = False  # if subset= true, use a subset for debugging.
 train_dl, valid_dl, test_dl = data_generator(sourcedata_path, targetdata_path, configs, training_mode, subset = subset)
 logger.debug("Data loaded ...")
 
+
+rtpt = RTPT(name_initials='MK', experiment_name=f'TFDC-{args.training_mode}-{args.pretrain_dataset}{f"2{args.target_dataset}"if args.training_mode == "fine_tune_test" else ""}', max_iterations=configs.num_epoch)
 # Load Model
+# Start the RTPT tracking
+rtpt.start()
+
 """Here are two models, one basemodel, another is temporal contrastive model"""
 TFC_model = TFC(configs).to(device)
 classifier = target_classifier(configs).to(device)
@@ -114,6 +122,6 @@ classifier_optimizer = torch.optim.Adam(classifier.parameters(), lr=configs.lr, 
 
 # Trainer
 Trainer(TFC_model, model_optimizer, classifier, classifier_optimizer, train_dl, valid_dl, test_dl, device,
-        logger, configs, experiment_log_dir, training_mode)
+        logger, configs, experiment_log_dir, training_mode,rtpt)
 
 logger.debug(f"Training time is : {datetime.now()-start_time}")
